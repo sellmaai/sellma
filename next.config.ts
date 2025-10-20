ef8807458925941a8a14faf7bbc9ad47045e3dcd
Vishnu Kartha <vishnukartha1@gmail.com>
2025-10-19 18:49:38 -0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This tells Next.js to completely ignore ESLint checks during 'next build'
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
